8f9e33e285a6dd72f73e407dd4b6cfaf49fe1e28
satyam <satyamkale271@gmail.com>
2025-02-18 23:36:30 +0530

    multiple return in go func

--- function.go
+++ function.go
@@ -14,10 +14,17 @@ func multiReturnValues() (int, int) {
 	return 1, 2
 }
 
+func vardicValues() (int, int) {
+	return 5, 7
+}
+
 func function() {
 	res := plus(1, 2)
 	fmt.Println("1+2 =", res)
 	res = plusPlus(1, 2, 3) // res =   // note res :=  it declared and initialise ,  res = it reassign the value, now res will overwrite the value of previous res with new one
 	fmt.Println("1+2+3 =", res)
-
+	a, b := multiReturnValues() // store multiple value from function
+	fmt.Println("res =", a, b)
+	_, c := vardicValues()
+	fmt.Println("res =", c) // If you only want a subset of the returned values, use the blank identifier _.
 }
